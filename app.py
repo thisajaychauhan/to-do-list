5f6d76717988c4cc57f7d277da0c78fe4f23a156
Ajay Chauhan <ajayc0343@gmail.com>
2023-08-22 19:53:17 +0530

    todo-list added

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from config import SQLALCHEMY_DATABASE_URI
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    is_done = db.Column(db.Boolean, default=False)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    note = request.form.get('note')
+    add_note = Task(note=note)
+    db.session.add(add_note)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:root@host:5432/todo-list'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
